ce2588c696d2cd55d930240078d7e3d097f2a5af
n1ved <nivedrsalini@gmail.com>
2023-07-06 17:13:17 +0530

    testimonials

--- src/components/footers/MiniCenteredFooter.js
+++ src/components/footers/MiniCenteredFooter.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./style.css";
 import tw from "twin.macro";
 import styled from "styled-components";
 import { Container as ContainerBase } from "components/misc/Layouts.js"
@@ -18,7 +19,7 @@ const LogoImg = tw.img`w-8`;
 const LogoText = tw.h5`ml-2 text-2xl font-black tracking-wider`;
 
 const LinksContainer = tw.div`mt-8 font-medium flex flex-wrap justify-center items-center flex-col sm:flex-row`
-const Link = tw.a`border-b-2 border-transparent hocus:text-gray-300 hocus:border-gray-300 pb-1 transition duration-300 mt-2 mx-4`;
+const Link = tw.a`border-b-2 border-transparent hocus:text-gray-300 hocus:border-gray-300 pb-1 transition duration-300 mt-2 mx-4 text-gray-200`;
 
 const SocialLinksContainer = tw.div`mt-10`;
 const SocialLink = styled.a`
